17ed443e05c27e8f79b080d99bc1b83d1c906742
komsh1l <132499758+komsh1l@users.noreply.github.com>
2023-12-25 22:11:27 +0300

    Delete Homework1 directory

--- Homework1/ch1/target/debug/ch1.d
+++
@@ -1,1 +0,0 @@
-C:\Users\ilyak\rust\mtuci-rs\ch1\target\debug\ch1.exe: C:\Users\ilyak\rust\mtuci-rs\ch1\src\main.rs
--- Homework1/ch1/target/debug/deps/ch1.d
+++
@@ -1,5 +0,0 @@
-C:\Users\ilyak\rust\mtuci-rs\ch1\target\debug\deps\ch1.exe: src\main.rs
-
-C:\Users\ilyak\rust\mtuci-rs\ch1\target\debug\deps\ch1.d: src\main.rs
-
-src\main.rs:
